7abc299cd129ac5d682e614675257d541018b55e
Leonardo <163869485+LeoMilare@users.noreply.github.com>
2025-01-29 10:26:18 -0300

    Add files via upload
    Foi feito um pequeno projeto/treino com a programação sem ser orientada a objetos, para calcular as áreas de 2 triângulos respectivamente e no final entregar qual área é maior. Um adendo foi, o uso de CultureInfo.InvariantCulture para entregar números com casas decimais com "." e não ","

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+class Program
+{
+    static void Main()
+    {
+        double xA, xB, xC, yA, yB, yC;
+
+        Console.WriteLine("Entre com as medidas do primeiro triângulo: ");
+        xA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        xB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        xC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Console.WriteLine("Entre com as medidas do segundo triângulo: ");
+        yA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        yB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        yC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        double p = (xA + xB + xC) / 2.0;
+        double AreaPrimeiroTriangulo = Math.Sqrt(p * (p - xA) * (p - xB) * (p - xC));
+
+        p = (yA + yB + yC) / 2.0;
+        double AreaSegundoTriangulo = Math.Sqrt(p * (p - yA) * (p - yB) * (p - yC));
+
+        Console.WriteLine("Área do primeiro triângulo é : " + AreaPrimeiroTriangulo.ToString("F4", CultureInfo.InvariantCulture));
+        Console.WriteLine("Área do segundo triângulo é : " + AreaSegundoTriangulo.ToString("F4", CultureInfo.InvariantCulture));
+
+        if (AreaPrimeiroTriangulo > AreaSegundoTriangulo) 
+        {
+            Console.WriteLine("Maior área é a do Primeiro Triangulo ");
+        }
+        else
+        {
+            Console.WriteLine("Maior área é a do Segundo Triangulo ");
+        }
+    }
+}
