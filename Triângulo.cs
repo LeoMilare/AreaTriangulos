662984c76a63d2533f65b5c1400e822e95088c3c
Leonardo <163869485+LeoMilare@users.noreply.github.com>
2025-01-29 11:53:39 -0300

    Add files via upload
    Usei a mesma base do código mas agora com a diferença de que eu criei uma classe Triangulo para armazenar mais atributos dentro dela e fazer com que seja mais "clean" para leitura e também treinar o uso de criação de outras classes e o uso de "public" e "new"

---
+++ Triângulo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Udemy___Área_de_triângulo
+{
+    internal class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+    }
+}
